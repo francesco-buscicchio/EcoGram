cb34628df935f80cc6ca958d81dd4ac51c39e354
francesco-buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2023-07-28 14:50:05 +0200

    Bug KeyBoard on Login

--- lib/Screens/Login/login_screen.dart
+++ lib/Screens/Login/login_screen.dart
@@ -7,7 +7,7 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(body: Body());
+    return const Scaffold(body: Body(),resizeToAvoidBottomInset: false,);
   }
 }
 
