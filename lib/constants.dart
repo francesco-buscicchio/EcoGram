3e03fe8e16f18a09448d41bfaec9188d8e79a90a
Francesco Buscicchio <81358077+francesco-buscicchio@users.noreply.github.com>
2023-10-11 20:39:06 +0200

    Fix #8

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 const kPrimaryColor = Color(0xFF6F35A5);
 const kPrimaryLightColor = Color(0xFFF1E6FF);
+String kApiKey = dotenv.env['FLUTTER_API_KEY'] ?? '';
--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:ecogram/Screens/Homepage/homepage.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 Future<void> main() async {
+    await dotenv.load();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
